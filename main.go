82939e3a3f549ef3d7b720eaf3a5d6f0b7126670
AvelarJ <javelar@uwo.ca>
2026-04-28 13:52:39 -0400

    Added register handler and incorporated login handler to check if given user has been registered

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/google/uuid"
+
+	_ "github.com/lib/pq"
+
+	"database/sql"
+
+	"github.com/AvelarJ/Gator/internal/database"
 
 	"github.com/AvelarJ/Gator/internal/config"
 )
 
 type state struct {
-	*config.Config
+	db  *database.Queries
+	cfg *config.Config
 }
 
 type command struct {
@@ -34,15 +45,60 @@ func (c *commands) register(name string, f func(*state, command) error) {
 	c.Handlers[name] = f
 }
 
+// Handler functions for each command
+
+// Register a new user if not already in db
+func handlerRegister(s *state, cmd command) error {
+	if len(cmd.Args) < 1 {
+		return fmt.Errorf("username required")
+	}
+
+	ctx := context.Background()
+
+	uuid := uuid.New()
+	username := sql.NullString{String: cmd.Args[0], Valid: true}
+	//Check if username already exists
+	oldUser, err := s.db.GetUser(ctx, username)
+	if err == nil && oldUser.Name == username {
+		fmt.Println("user already exists")
+		os.Exit(1)
+	}
+
+	currUser, err := s.db.CreateUser(ctx, database.CreateUserParams{
+		ID:        uuid,
+		Name:      username,
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+	if err != nil {
+		return fmt.Errorf("error creating user: %w", err)
+	}
+
+	s.cfg.SetUser(currUser.Name.String)
+	fmt.Println("Registered as", currUser.Name.String)
+	fmt.Println(currUser)
+	return nil
+}
+
+// Login an existing user
 func handlerLogin(s *state, cmd command) error {
 	if len(cmd.Args) < 1 {
 		return fmt.Errorf("username required")
 	}
 
-	username := cmd.Args[0]
-	s.Config.SetUser(username)
+	username := sql.NullString{String: cmd.Args[0], Valid: true}
 
-	fmt.Println("Logged in as", username)
+	ctx := context.Background()
+
+	_, err := s.db.GetUser(ctx, username)
+	if err != nil {
+		fmt.Println("user does not exist")
+		os.Exit(1)
+	}
+
+	s.cfg.SetUser(username.String)
+
+	fmt.Println("Logged in as", username.String)
 	return nil
 }
 
@@ -53,11 +109,25 @@ func main() {
 		fmt.Println("Error reading config:", err)
 	}
 
-	conf_state := state{Config: &conf}
+	//conf.Database_url = "postgres://jordanavelar:@localhost:5432/gator"
+
+	// Opening a postgres database connection from the config
+	db, err := sql.Open("postgres", conf.Database_url)
+	if err != nil {
+		fmt.Println("Error opening database:", err)
+		os.Exit(1)
+	}
+	defer db.Close()
+
+	// New database queries struct
+	dbQueries := database.New(db)
+
+	conf_state := state{db: dbQueries, cfg: &conf}
 
 	// Create a fresh commands struct
 	cmds := commands{Handlers: make(map[string]func(*state, command) error)}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	// Parse command line arguments
 	args := os.Args
